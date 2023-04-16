0cf6942d3a922a56eeba51268d4129040727378b
kalyan <nyshadhamkrishnakalyan2@gmail.com>
2023-04-16 19:10:38 +0530

    change

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-let text
+const text = document.querySelector(".sec-text");
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Vivitsu";
+    }, 0);
+}
+textLoad();
+setInterval(textLoad, 12000);
